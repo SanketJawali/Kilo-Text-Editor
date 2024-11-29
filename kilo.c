9e4771298331339b8a53cc17b328b91cb8b0f0dd
root <root@Victus25.>
2024-11-29 17:54:51 +0530

    Added ~ at the beginning of the lines

--- kilo.c
+++ kilo.c
@@ -64,9 +64,22 @@ char editorReadKey() {
 
 
 /*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen() {
     // Clear the screen
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    // Draw the ~
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 
